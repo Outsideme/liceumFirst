a5af4f68673567c4219b1dca2c0c4b2eb0fdfab5
Маловичко Иван Сергеевич <malovichkois@ul-lyceum.ru>
2023-01-31 17:52:32 +0300

    news

--- src/js/app.js
+++ src/js/app.js
@@ -2536,11 +2536,19 @@ if (sessionStorage.getItem(4) === "bigDistance") {
   fontsCharger.biggestDistance();
 }
 //=============================================//=============================================//============================================>>
-import { Swiper, Navigation, Pagination, EffectCards } from "swiper";
-
+import {
+  Swiper,
+  Navigation,
+  Pagination,
+  EffectCards,
+  Thumbs,
+  Lazy,
+} from "swiper";
 import "swiper/css/bundle";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+Swiper.use([Thumbs]);
+Swiper.use([Lazy]);
 Swiper.use([Navigation]);
 Swiper.use([Pagination]);
 Swiper.use([EffectCards]);
@@ -2882,6 +2890,16 @@ new Swiper(".swiper-container-10", {
   // loop: true,
   grabCursor: true,
 });
+// document.querySelectorAll(".swiper-slide-news").forEach((elem) => {
+//   setTimeout(() => {
+//     if (elem.classList.contains("swiper-slide-active")) {
+//       elem.classList.add("qqqwww");
+//       console.log(elem);
+//     } else if (!elem.classList.contains("swiper-slide-active")) {
+//       elem.classList.remove("qqqwww");
+//     }
+//   }, 0);
+// });
 
 if (document.querySelector(".infrastructure__li-1")) {
   document
@@ -3656,6 +3674,61 @@ const hideTeam = () => {
   ${document.title}`;
 })();
 
+// Инициализация превью слайдера
+const sliderThumbs = new Swiper(".slider__thumbs .swiper-container", {
+  // ищем слайдер превью по селектору
+  // задаем параметры
+  direction: "vertical", // вертикальная прокрутка
+  slidesPerView: 3, // показывать по 3 превью
+  spaceBetween: 24, // расстояние между слайдами
+  navigation: {
+    // задаем кнопки навигации
+    nextEl: ".slider__next", // кнопка Next
+    prevEl: ".slider__prev", // кнопка Prev
+  },
+  freeMode: true, // при перетаскивании превью ведет себя как при скролле
+  breakpoints: {
+    // условия для разных размеров окна браузера
+    0: {
+      // при 0px и выше
+      direction: "horizontal", // горизонтальная прокрутка
+    },
+    768: {
+      // при 768px и выше
+      direction: "vertical", // вертикальная прокрутка
+    },
+  },
+});
+// Инициализация слайдера изображений
+const sliderImages = new Swiper(".slider__images .swiper-container", {
+  // ищем слайдер превью по селектору
+  // задаем параметры
+  direction: "vertical", // вертикальная прокрутка
+  slidesPerView: 1, // показывать по 1 изображению
+  spaceBetween: 32, // расстояние между слайдами
+  mousewheel: true, // можно прокручивать изображения колёсиком мыши
+  navigation: {
+    // задаем кнопки навигации
+    nextEl: ".slider__next", // кнопка Next
+    prevEl: ".slider__prev", // кнопка Prev
+  },
+  grabCursor: true, // менять иконку курсора
+  thumbs: {
+    // указываем на превью слайдер
+    swiper: sliderThumbs, // указываем имя превью слайдера
+  },
+  breakpoints: {
+    // условия для разных размеров окна браузера
+    0: {
+      // при 0px и выше
+      direction: "horizontal", // горизонтальная прокрутка
+    },
+    768: {
+      // при 768px и выше
+      direction: "vertical", // вертикальная прокрутка
+    },
+  },
+});
 //============================/===================/====================>
 
 /*
